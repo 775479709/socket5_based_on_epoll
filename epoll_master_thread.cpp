eb4426b0eef1a70637703221a254b28e9aae36f7
775479709 <775479709@qq.com>
2018-11-08 04:22:50 -0800

    update2.0

--- epoll_master_thread.cpp
+++ epoll_master_thread.cpp
@@ -1,6 +1,6 @@
 #include "epoll_master_tread.h"
 
-EpollMasterThread::EpollMasterThread{
+EpollMasterThread::EpollMasterThread(const char *ip, int port, size_t work_thread_num = 2){
     listen_fd_ = socket(AF_INET, SOCK_STREAM, 0);
     assert(listen_fd_ > 0);
     bzero(&local_address_, sizeof(local_address_)); 
