d16ae3506c59a2693bb5d228e9f210ef370219d9
775479709 <775479709@qq.com>
2018-11-29 22:28:12 +0800

    test memory pool

--- memory_pool_test.cpp
+++ memory_pool_test.cpp
@@ -1,8 +1,46 @@
 #include "memory_pool.h"
+#include<stdlib.h>
+#include<sys/time.h>
 
-int main()
+int myrand(int l,int r){
+    return rand()%(r-l+1) +l;
+}
+
+int main(int argc, char **argv)
 {
-    MemoryPool<1024> *memory_pool = new MemoryPool<1024>();
-    
+    srand(time(0));
+    timeval start, end;
+    if(argc > 1) {
+        MemoryPool<1024>* memory_pool = new MemoryPool<1024>();
+        long long all_1 = 0;
+        gettimeofday(&start, 0);
+        for(int i = 0; i < 1000000;i++) {
+            size_t size = myrand(1,800);
+            char *f = (char *)memory_pool->Malloc(size);
+            all_1 += size;
+            memory_pool->Free((void *)f);
+        }
+        gettimeofday(&end, 0);
+        int t1 = 1000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+        printf("allsize = %lld,time =%d\n",all_1,t1);
+        delete memory_pool;
+    }else {
+        long long all_1 = 0;
+        gettimeofday(&start, 0);
+        for(int i = 0; i < 1000000;i++) {
+            size_t size = myrand(1,800);
+            char *f = new char[size];
+            all_1 += size;
+            delete [] f;
+        }
+        gettimeofday(&end, 0);
+        int t1 = 1000*(end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec);
+        printf("allsize = %lld,time =%d\n",all_1,t1);
+    }
+
+
+
+
+
     return 0;
 }
